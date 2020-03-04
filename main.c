145f9e36c9d79f4c55320d4d51c4c2e19bd93c3a
Slava <moiseev12348@gmail.com>
2020-03-04 17:59:15 +0700

    Conflict fix

--- main.c
+++ main.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 int min_element(int* arr, int size)
 {
     int min = *arr;
-
     int* it;
     for (it = arr + 1; it < arr + size; ++it) {
         if (*it < min) {
             min = *it;
         }
     }
-
     return min;
 }
 
@@ -18,16 +15,7 @@ int main()
 {
     int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, 7);
-
-    printf("Array: ");
-    int i;
-    for (i = 0; i < 7; ++i) {
-        printf("%d ", arr[i]);
-    }
     printf("\n");
 
-    printf("Minimum element: %d\n", min);
-
-    return 0;
+    printf("Minimum element: %d\n", min) return 0;
 }
